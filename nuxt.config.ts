d6c095da777ec789b28339d6c7249a894a097095
Nicolas Walcker <walcker_@outlook.com>
2023-11-26 11:08:30 -0300

    chore: update pwa funcionality

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,10 +13,42 @@ export default defineNuxtConfig({
   ],
   pwa: {
     registerType: 'autoUpdate',
+    injectRegister: 'script',
     manifest: {
       name: 'todocise',
       short_name: 'todocise',
-      theme_color: '#ffffff'
+      theme_color: '#09090B',
+      background_color: '#09090B',
+      display: 'standalone',
+      scope: '/',
+      start_url: '/',
+      description: 'A todo app with a twist',
+      icons: [
+        {
+          src: '/icon-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+          purpose: 'maskable any'
+        },
+        {
+          src: '/icon-256x256.png',
+          sizes: '256x256',
+          type: 'image/png',
+          purpose: 'maskable any'
+        },
+        {
+          src: '/icon-384x384.png',
+          sizes: '384x384',
+          type: 'image/png',
+          purpose: 'maskable any'
+        },
+        {
+          src: '/icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'maskable any'
+        }
+      ]
     },
     workbox: {
       globPatterns: ['**/*.{js,css,html,png,svg,ico}']
